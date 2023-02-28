3556a344d80b11c15e86856942b5e709c2b72981
Jingbo Su <sujingbo0217@gmail.com>
2023-02-28 23:49:53 +0800

    Add database queries and pass tests

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
 const connectDB = (url) => {
+  console.log('DataBase Connecting...')
   return mongoose.connect(url)
 }
 
--- server/routes/checker.js
+++ server/routes/checker.js
@@ -3,15 +3,16 @@ const router = express.Router()
 
 const {
   createArticle,        // after user click the submit button
-  getAllArticles,       // after user login / refresh / back the page
+  getAllArticlesId,     // after user login / refresh / back the page (displayed at sidebar)
+  getAnArticle,         // after user click one article of the sidebar articles
   createResponse,       // after creating article or updating article
   getResponse,          // after GPT create the response
   updateArticle,        // after user click the update button
   deleteArticle         // after user click the delete button
 } = require('../controllers/checker')
 
-router.route('/').post(createArticle).get(getAllArticles)
-router.route('/:id').patch(updateArticle).delete(deleteArticle)
+router.route('/').post(createArticle).get(getAllArticlesId)
+router.route('/:id').get(getAnArticle).patch(updateArticle).delete(deleteArticle)
 router.route('/:resp').post(createResponse).get(getResponse)
 
 module.exports = router
